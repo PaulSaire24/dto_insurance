b7ef49cbdf2f3046bb41a6eaceee636f4b0c4477
jose.sandoval.tirado <jose.sandoval.tirado@bbva.com>
2024-01-29 15:23:08 -0500

    PERAR-25814: FEAT!: modify participant dto and add utils package

--- src/main/java/com/bbva/rbvd/dto/insurance/commons/AddressesDTO.java
+++ src/main/java/com/bbva/rbvd/dto/insurance/commons/AddressesDTO.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class AddressesDTO implements Serializable {
     private LocationDTO location;
+    private String formattedAddress;
+    private AddressTypeDTO addressType;
 
     public LocationDTO getLocation() {
         return location;
@@ -11,6 +13,22 @@ public class AddressesDTO implements Serializable {
 
     public void setLocation(LocationDTO location) {
         this.location = location;
+    }
+
+    public String getFormattedAddress() {
+        return formattedAddress;
+    }
+
+    public void setFormattedAddress(String formattedAddress) {
+        this.formattedAddress = formattedAddress;
+    }
+
+    public AddressTypeDTO getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(AddressTypeDTO addressType) {
+        this.addressType = addressType;
     }
 
     @Override
--- src/main/java/com/bbva/rbvd/dto/insurance/commons/LocationDTO.java
+++ src/main/java/com/bbva/rbvd/dto/insurance/commons/LocationDTO.java
@@ -4,30 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 public class LocationDTO implements Serializable {
-    private List<AddressComponentsDTO> addressComponents;
-    private String formattedAddress;
+    private List<AddressComponentsDTO> addressComponent;
+    private List<String> locationTypes;
 
-    public List<AddressComponentsDTO> getAddressComponents() {
-        return addressComponents;
-    }
 
-    public void setAddressComponents(List<AddressComponentsDTO> addressComponents) {
-        this.addressComponents = addressComponents;
-    }
-
-    public String getFormattedAddress() {
-        return formattedAddress;
-    }
-
-    public void setFormattedAddress(String formattedAddress) {
-        this.formattedAddress = formattedAddress;
-    }
-
-    @Override
-    public String toString() {
-        return "LocationDTO{" +
-                "addressComponents=" + addressComponents +
-                ", formattedAddress='" + formattedAddress + '\'' +
-                '}';
-    }
 }
